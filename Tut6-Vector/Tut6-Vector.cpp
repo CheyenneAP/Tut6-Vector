e17ad3a35d45e14bccb67d9785a4eead0678265a
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-04-10 16:56:29 +0200

    generates random number to populate the vector, sorts the vector using the STL, reverses the order to display the vector in descending order  using STL

--- Tut6-Vector/Tut6-Vector.cpp
+++ Tut6-Vector/Tut6-Vector.cpp
@@ -1,9 +1,55 @@
+// Cheyenne Pillay
+// 214513360
+// Tut6-Vector
+
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <ctime>
 using namespace std;
+
+
+// initialises the method
+void disp(vector <int>);
 
 
 int main(){
 
+
+	//initialises the vector
+  vector <int> vec;
+	srand(time(NULL));
+
+	int randNum; // variable to hold the random number 
+
+	for (int i = 0; i < 20; i++){
+		// generates a random number  between 1 and 100
+		randNum = 1+rand() % 100 ;
+
+		 // pushes 20 random values into the vector 
+		vec.push_back(randNum);
+	}
+
+	// displays the unsorted vector
+	cout << "unsorted array" << endl;
+	cout << endl;
+	disp(vec);
+	cout <<"______________________________________________________________________________" <<endl;
+
+
+	// sorts the vector into ascending order
+	sort(vec.begin(), vec.end());
+	// reverses the order of the numbers in the vector to display in descending order
+	reverse(vec.begin(), vec.end());
+	// displays the sorted vector in descending order
+	cout << "sorted array" << endl;
+	cout << endl;
+    disp(vec);
+	cout << "______________________________________________________________________________" << endl;
+
+
 }
+
+// method to dispay the vector
+  
